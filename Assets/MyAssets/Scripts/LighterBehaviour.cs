1347a43146fcc94fd90ad0f20c553a52df76ee1f
yoshiki-yoshiki <i131320@gm.ishikawa-nct.ac.jp>
2014-05-14 17:56:05 +0900

    バグの修正
    ギミックを投げるのを繰り返すと遠くに飛んでしまうバグを修正
    ライターのギミックの追加
    ドラム缶に引火、爆発を追加

---
+++ Assets/MyAssets/Scripts/LighterBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ライターのふるまいを定義するクラス
+/// </summary>
+public class LighterBehaviour : BaseGimmicBehaviour {
+    public static bool Lighter { get; private set; }
+    public GameObject Fire;
+    public static GameObject Fire_obj { get; private set; }
+    public static GameObject lighter { get; private set; }
+
+	// Use this for initialization
+	void Start () 
+    {
+        lighter = this.gameObject;
+        Lighter = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (IsGripped)
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localEulerAngles = Vector3.zero;
+
+            if (Input.GetButtonDown("PickUp"))
+            {
+                if (Lighter)
+                {
+                    Lighter = false;
+                    Destroy(Fire_obj);
+                }
+                else
+                {
+                    Lighter = true;
+                    Fire_obj = Instantiate(Fire, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+                    Fire_obj.transform.parent = transform;
+                }
+            }
+        }
+	}
+
+}
